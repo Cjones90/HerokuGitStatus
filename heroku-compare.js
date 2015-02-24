85bb659af0887ae5cb031f7fd5e4c1b7a568c216
KC <kcjones.2002@gmail.com>
2015-02-24 00:06:15 -0800

    add some options

--- heroku-compare.js
+++ heroku-compare.js
@@ -14,7 +14,7 @@ github.authenticate({
     token: gitToken
 });
 
-var ORG_NAME = "NAQ";
+
 //Change Organization to yours (added functionality soon to come);
 
 var heroRepoNames = require('./heroku_github_namemap.json');
@@ -40,9 +40,21 @@ String.prototype.padRight = function (len) {
 
 ///TODO implement error catching
 
-function getGit (callback) {
+function getGit (options, callback) {
   var gitRepos = [];
-  github.repos.getFromOrg({org: ORG_NAME}, function (err, repos) {
+  if(options.userFlag === "user") {
+    getRepos = github.repos.getFromUser;
+    username = {user: options.userName};
+  }
+  else if (options.userFlag === "org") {
+    getRepos = github.repos.getFromOrg;
+    username = {org: options.userName};
+  }
+  else {
+    console.log("Please be sure to enter a userFlag and userName in the options parameter");
+    return;
+  }
+  getRepos(username, function (err, repos) {
     repos.forEach(function (repo) {
       github.repos.getBranches({user: repo.owner.login, repo: repo.name}, function (err, branches) {
         branches.forEach(function (branch) {
@@ -74,12 +86,12 @@ function getHeroku(callback) {
   })
 }
 
-function getRepos(git, heroku, cb) {
+function getRepos(userFlag, git, heroku, cb) {
   var REPO = {
     gitArr: [],
     heroArr: []
   }
-  git(function (err, gitRepos) {
+  git(userFlag, function (err, gitRepos) {
     gitRepos.forEach(function (repo) {
       REPO.gitArr[REPO.gitArr.length] = repo;
       if(REPO.gitArr.length === gitRepos.length) {
@@ -102,8 +114,23 @@ function getRepos(git, heroku, cb) {
 
 ////////////^^^ FUNCTIONS//////// CALLS
 
+/* userFlag options: (string)
+* "user: github.repos.getFromUser function
+* "org": github.repos.getFromOrg  function
+*
+* userName options: (string)
+* "yourusername": get repos for "yourusername" 
+* (used with user userFlag)
+* "yourorgname": get repos from your organization on github "yourorgname" 
+* (used with org userFlag)
+*/
 
-getRepos(getGit, getHeroku, function (repos) {
+var options = {
+  userFlag: "org",
+  userName: "NAQ"
+}
+
+getRepos(options, getGit, getHeroku, function (repos) {
   //Result contains all repos from git/heroku as an object in the form of:
   //  repos = {gitArr: [{}, {} ...], heroArr: [{}, {} ...]}
   //Git array = [{name: "RepoName", gitCommit: "7 dig Sha Number"}, {}, {} ...]
