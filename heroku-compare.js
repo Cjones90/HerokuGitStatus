0dcef7699bf5d269f132071a1b8c04f546cf6fd8
KC <kcjones.2002@gmail.com>
2015-02-24 22:51:57 -0800

    fix options, add semi-dynamic padding

--- heroku-compare.js
+++ heroku-compare.js
@@ -15,8 +15,6 @@ github.authenticate({
 });
 
 
-//Change Organization to yours (added functionality soon to come);
-
 var heroRepoNames = require('./heroku_github_namemap.json');
 /*Map heroku apps to github repos in the file above like so
 {
@@ -24,10 +22,6 @@ var heroRepoNames = require('./heroku_github_namemap.json');
   "HerokuApp2": "MatchingGitHubApp"
 }
 */
-
-var REPONAME_PAD_RIGHT_LENGTH = 24;
-// Change to pad repo names to desirable length when logged in console
-// TODO Add functionalitiy to adjust console.log(--Repo--)
 
 String.prototype.padRight = function (len) {
   if(this.length < len) {
@@ -42,24 +36,22 @@ String.prototype.padRight = function (len) {
 
 function getGit (options, callback) {
   var gitRepos = [];
-  if(options.userFlag === "user") {
+  if(options.user) {
     getRepos = github.repos.getFromUser;
-    username = {user: options.userName};
   }
-  else if (options.userFlag === "org") {
+  else if (options.org) {
     getRepos = github.repos.getFromOrg;
-    username = {org: options.userName};
   }
   else {
-    console.log("Please be sure to enter a userFlag and userName in the options parameter");
+    console.log("Please be sure to enter the appropriate options");
     return;
   }
-  getRepos(username, function (err, repos) {
+  getRepos(options, function (err, repos) {
     repos.forEach(function (repo) {
       github.repos.getBranches({user: repo.owner.login, repo: repo.name}, function (err, branches) {
         branches.forEach(function (branch) {
           if(branch.name === 'master') {
-            gitRepos[gitRepos.length] = {name: repo.name, gitCommit: branch.commit.sha.slice(0,7), branchCount: branches.length-1}
+            gitRepos[gitRepos.length] = {name: repo.name, commit: branch.commit.sha.slice(0,7), branchCount: branches.length-1}
           }
         })
         if(gitRepos.length === repos.length) {
@@ -76,7 +68,7 @@ function getHeroku(callback) {
     for(var i in apps) {
       (function (i) {
         heroku.get_releases(apps[i].name, function (err, res) {
-          herokuRepos[herokuRepos.length] = {name: apps[i].name, herokuCommit: res[res.length-1].commit}
+          herokuRepos[herokuRepos.length] = {name: apps[i].name, commit: res[res.length-1].commit}
           if(herokuRepos.length === apps.length) {
             callback(null, herokuRepos);
           }
@@ -111,39 +103,52 @@ function getRepos(userFlag, git, heroku, cb) {
 
 }
 
+function padRepoLength(repos) {
+  var padRepo = {
+    MAX_LENGTH: 0,
+    str: ''
+  }
+  for(p in repos) {
+    if(repos[p].length > padRepo.MAX_LENGTH) {
+      padRepo.MAX_LENGTH = repos[p].length;
+    }
+  }
+  for(var i = 0; i < (padRepo.MAX_LENGTH/2)-2; i++) {
+    padRepo.str += "-";
+  }
+  padRepo.str += 'Repo'
+  for(var j = 0; j < (padRepo.MAX_LENGTH/2)-2; j++) {
+    padRepo.str += "-";
+  }
+  return padRepo;
+}
+
 
 ////////////^^^ FUNCTIONS//////// CALLS
 
-/* userFlag options: (string)
-* "user: github.repos.getFromUser function
-* "org": github.repos.getFromOrg  function
-*
-* userName options: (string)
-* "yourusername": get repos for "yourusername" 
-* (used with user userFlag)
-* "yourorgname": get repos from your organization on github "yourorgname" 
-* (used with org userFlag)
+/*
+ options: {object}
+ {org: "orgName"}
+ {user: "username"}
 */
 
-var options = {
-  userFlag: "org",
-  userName: "NAQ"
-}
+var options = {org: "NAQ"};
+// var options = {user: "Cjones90"};
 
 getRepos(options, getGit, getHeroku, function (repos) {
   //Result contains all repos from git/heroku as an object in the form of:
   //  repos = {gitArr: [{}, {} ...], heroArr: [{}, {} ...]}
-  //Git array = [{name: "RepoName", gitCommit: "7 dig Sha Number"}, {}, {} ...]
-  //Heroku array = [{name: "RepoName", herokuCommit: "7 dig Sha Number"}, {}, {} ...]
-  // TODO make gitCommit and herokuCommit = commit
+  //Git array = [{name: "RepoName", commit: "7 dig Sha Number"}, {}, {} ...]
+  //Heroku array = [{name: "RepoName", commit: "7 dig Sha Number"}, {}, {} ...]
   if(repos.gitArr.length && repos.heroArr.length) {
+    var maxRepo = padRepoLength(heroRepoNames);
     console.log(); //// Buffer
-    console.log("---------- Repo -------- ||  ---- Git ---  ||  -- Heroku -- ");
+    console.log(maxRepo.str+" ||  ---- Git ---  ||  -- Heroku -- ");
     repos.gitArr.forEach(function (gitRepo) {
       repos.heroArr.forEach(function (heroRepo) {
         if(gitRepo.name === heroRepoNames[heroRepo.name]) {
-          var str = gitRepo.name.padRight(REPONAME_PAD_RIGHT_LENGTH)+" ||  Sha: "+ gitRepo.gitCommit+"  ||  Sha: "+ heroRepo.herokuCommit+ "  === ";
-          var flag = gitRepo.gitCommit === heroRepo.herokuCommit ? str.green+"Match".padRight(12).green : str.red+"OUT OF DATE".padRight(12).red;
+          var str = gitRepo.name.padRight(maxRepo.MAX_LENGTH)+" ||  Sha: "+ gitRepo.commit+"  ||  Sha: "+ heroRepo.commit+ "  === ";
+          var flag = gitRepo.commit === heroRepo.commit ? str.green+"Match".padRight(12).green : str.red+"OUT OF DATE".padRight(12).red;
           flag = (gitRepo.branchCount > 0) ? flag+String("== "+gitRepo.branchCount).yellow+" branch(es)".yellow : flag;
           console.log(flag);
         }
